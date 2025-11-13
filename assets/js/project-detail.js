849efe4c1783707476a2fe14a30fa68e6909216d
Hüseyin Balık <huseyinsbalik@gmail.com>
2025-11-13 23:47:59 +0300

    project img added

--- assets/js/project-detail.js
+++ assets/js/project-detail.js
@@ -11,14 +11,14 @@
   // Galeri değişkenleri
   var currentImageIndex = 0;
   var images = [
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg',
-    'assets/images/proje.jpeg'
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
+    'assets/images/7.jpg',
+    'assets/images/8.jpg'
   ];
 
   // Ana görseli değiştir
